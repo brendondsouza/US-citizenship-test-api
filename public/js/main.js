6030a7de39c52eccec4f94b59520898c9ddca7b2
Brendon DSouza <brendonmufc@gmail.com>
2022-06-10 17:13:20 -0400

    testing on page requests

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+document.querySelector('#request').addEventListener('click', getQuestion)
+const url = 'https://american-citizenship-test.herokuapp.com/api/'
+
+function getQuestion(){
+  let choice = document.querySelector('input').value.toLowerCase()
+  
+  console.log(`${url}${choice}`)
+  fetch(`${url}${choice}`)
+  .then(res => res.json()) // parse response as JSON
+  .then(data => {
+    console.log(data)
+  
+  })
+  .catch(err => {
+      console.log(`error ${err}`)
+  });
+}
