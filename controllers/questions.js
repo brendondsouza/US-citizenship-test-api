dea4b69a771678044766eafbd1ab76f74dffa280
Brendon DSouza <brendonmufc@gmail.com>
2022-10-23 15:26:40 -0400

    modified api controller login

--- controllers/questions.js
+++ controllers/questions.js
@@ -12,7 +12,8 @@ module.exports = {
     getApiRandom: async (req, res)=>{
         try{
             const questions = await Question.find()
-            res.render('questions.ejs', {questions: questions})
+            const random = Math.ceil(Math.random()*100)-1
+            res.render('questions.ejs', {questions: questions[random]})
         }catch(err){
             console.log(err)
         }
@@ -28,7 +29,8 @@ module.exports = {
     getGovernmentRandom: async (req, res)=>{
         try{
             const questions = await Question.find({topic:"American Government"})
-            res.render('questions.ejs', {questions: questions})
+            const random = Math.ceil(Math.random()*57)-1
+            res.render('questions.ejs', {questions: questions[random]})
         }catch(err){
             console.log(err)
         }
@@ -44,7 +46,8 @@ module.exports = {
   getHistoryRandom: async (req, res)=>{
       try{
         const questions = await Question.find({topic:"American History"})
-        res.render('questions.ejs', {questions: questions})
+        const random = (Math.ceil(Math.random()*30)-1) + 57
+        res.render('questions.ejs', {questions: questions[random]})
       }catch(err){
           console.log(err)
       }
@@ -59,8 +62,10 @@ module.exports = {
 },
 getCivicsRandom: async (req, res)=>{
     try{
-        const questions = await Question.find()
-        res.render('questions.ejs', {topic: "Integrated Civics"})
+        const questions = await Question.find({topic: "Integrated Civics"})
+        const random = (Math.ceil(Math.random()*12)-1) + 87
+        res.render('questions.ejs', {questions: questions[random]})
+        
     }catch(err){
         console.log(err)
     }
